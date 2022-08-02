5341c5794bfd10e79f0e836f6896dc6e3ef76d16
Andrei Kagu <andreikagu03@gmail.com>
2022-08-02 10:13:53 -0700

    [Feature3] Added 1000-7 meme

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Project
 {
@@ -10,8 +10,21 @@ namespace Project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
-            Console.ReadLine();
+            Console.WriteLine("Hello World!!!!!!!!!!");
+            int num = 1000;
+            Console.ForegroundColor = ConsoleColor.Green;
+            
+
+            for (int i = 0; i <= 1000; i++)
+            {
+                
+                num = num - 7;
+
+                Console.WriteLine(num);
+                Thread.Sleep(30);
+            }
+
+
         }
     }
 }
